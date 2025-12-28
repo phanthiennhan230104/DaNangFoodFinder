2f8955e2f439df8cf3aa844caae859982bf5c67d
phanthiennhan230104 <phanthiennhan230104@gmail.com>
2025-12-28 14:57:26 +0700

    Disable SSL for SMTP email backend configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -144,6 +144,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get("SMTP_EMAIL")
 EMAIL_HOST_PASSWORD = os.environ.get("SMTP_APP_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
